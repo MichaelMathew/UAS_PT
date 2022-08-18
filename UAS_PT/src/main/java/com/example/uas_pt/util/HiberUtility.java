53f757a86ce31fe391240a5d65de4f59fee6b4a4
Axel Shira Sapata Negara <axelshira88@gmail.com>
2022-08-18 16:15:24 +0700

    18/8/22-Kerangka

---
+++ UAS_PT/src/main/java/com/example/uas_pt/util/HiberUtility.java
@@ -0,0 +1,13 @@
+//package com.example.uas_pt.util;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//
+//
+//public class HiberUtility {
+//
+//    public static SessionFactory getSessionFactory() {
+//        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+//        return sessionFactory;
+//    }
+//}
