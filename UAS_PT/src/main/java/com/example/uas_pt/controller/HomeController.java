824804552dacbdc3395ce9a641d8f09ce8033233
MichaelMathew <2072007@maranatha.ac.id>
2022-08-23 18:02:53 +0700

    23/08/2022 Update fxml agar gambar dapat muncul saat di run
    Update fxml

--- UAS_PT/src/main/java/com/example/uas_pt/controller/HomeController.java
+++ UAS_PT/src/main/java/com/example/uas_pt/controller/HomeController.java
@@ -1,4 +1,22 @@
 package com.example.uas_pt.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+
 public class HomeController {
+    public ImageView release1;
+    @FXML
+    private ImageView release2;
+
+    public void initialize(){
+
+
+    }
 }
