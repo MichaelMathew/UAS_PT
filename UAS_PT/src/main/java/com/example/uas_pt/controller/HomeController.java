05ddda39e7804440693e50bd22fd606c91540708
MichaelMathew <2072007@maranatha.ac.id>
2022-08-24 14:08:35 +0700

    24/08/2022 Update Controller

--- UAS_PT/src/main/java/com/example/uas_pt/controller/HomeController.java
+++ UAS_PT/src/main/java/com/example/uas_pt/controller/HomeController.java
@@ -1,12 +1,19 @@
 package com.example.uas_pt.controller;
 
+import com.example.uas_pt.HelloApplication;
 import com.example.uas_pt.dao.BookDao;
 import com.example.uas_pt.model.BookEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class HomeController {
     public ImageView release1;
@@ -16,16 +23,25 @@ public class HomeController {
     public Label judul4;
     public Label judul5;
     public Label judul6;
+    public StackPane Content;
     @FXML
     private ImageView release2;
 
     ObservableList<BookEntity> buku;
 
     public void initialize(){
-
+        judul1 = new Label();
+        judul2 = new Label();
         BookDao dao = new BookDao();
-        buku = FXCollections.observableArrayList(dao.getJudul());
-//        String buku1 = String.valueOf(buku.indexOf(1));
+        buku = FXCollections.observableArrayList(dao.getData());
+        judul1.setText(buku.get(0).getTitle());
+        judul2.setText(buku.get(1).getTitle());
+    }
 
+    public void pressimage1(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("detail.fxml"));
+        Parent fxml = fxmlLoader.load();
+        Content.getChildren().removeAll();
+        Content.getChildren().setAll(fxml);
     }
 }
