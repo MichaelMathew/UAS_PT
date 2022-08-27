70bfa4a60713230eaa718b7ca062aef850ca0954
MichaelMathew <2072007@maranatha.ac.id>
2022-08-27 23:23:12 +0700

    27/08/2022 Update Controller dan Dao

--- UAS_PT/src/main/java/com/example/uas_pt/controller/ProfileController.java
+++ UAS_PT/src/main/java/com/example/uas_pt/controller/ProfileController.java
@@ -1,9 +1,26 @@
 package com.example.uas_pt.controller;
 
+import com.example.uas_pt.dao.UserDao;
+import com.example.uas_pt.model.UserEntity;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
 public class ProfileController {
 
-    public void data(){
+    public StackPane Content;
+    public Label name;
+    public Label email;
+    public ImageView lsimage;
+    public Label judul;
+    public Label genre;
+    public Label deskripsi;
 
+    UserEntity user;
+
+    public void data(Integer id){
+        UserDao dao = new UserDao();
+        user = dao.filterDataIdUser(id);
     }
 }
 
