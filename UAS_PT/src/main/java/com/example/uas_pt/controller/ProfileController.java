ed31ce4b13cc544b2d69b7fe63ecf3ef9c5b3617
MichaelMathew <2072007@maranatha.ac.id>
2022-08-28 23:17:58 +0700

    28/08/2022 Update Profile Controller

--- UAS_PT/src/main/java/com/example/uas_pt/controller/ProfileController.java
+++ UAS_PT/src/main/java/com/example/uas_pt/controller/ProfileController.java
@@ -1,14 +1,20 @@
 package com.example.uas_pt.controller;
 
 import com.example.uas_pt.HelloApplication;
+import com.example.uas_pt.dao.HistoryDao;
 import com.example.uas_pt.dao.UserDao;
+import com.example.uas_pt.model.HistoryEntity;
 import com.example.uas_pt.model.UserEntity;
 import com.google.gson.Gson;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
@@ -31,12 +37,11 @@ public class ProfileController {
 
     UserEntity user;
 
-    public Integer IdUser;
-
-    private LoginController loginController;
+    ObservableList<HistoryEntity> fhistory;
 
 
     public void initialize(){
+        HistoryDao hdao = new HistoryDao();
         UserDao dao = new UserDao();
         BufferedReader reader;
         String filename = "User/data.txt";
@@ -47,6 +52,16 @@ public class ProfileController {
             UserEntity dataUser = g.fromJson(json, UserEntity.class);
             name.setText(dataUser.getName());
             email.setText(dataUser.getEmail());
+            fhistory = FXCollections.observableArrayList(hdao.filterData(dataUser.getIdUser()));
+            int lastidx = 0;
+            for (HistoryEntity h: fhistory) {
+                lastidx = lastidx + 1;
+            }
+            int lastidx2 = lastidx - 1;
+            Image image = new Image(String.valueOf(getClass().getResource("/assets/" + fhistory.get(lastidx2).getBookIdBook() + ".jpg")));
+            lsimage.setImage(image);
+            judul.setText(fhistory.get(lastidx2).getBookByBookIdBook().getTitle());
+            genre.setText(fhistory.get(lastidx2).getBookByBookIdBook().getGenreByGenreIdGenre().getNamaGenre());
             reader.close();
             logout.setOnAction(actionEvent-> {
                 try {
