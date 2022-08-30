abe80bc42227667cf5e43617d7cc584c8ffe7ac5
MichaelMathew <2072007@maranatha.ac.id>
2022-08-31 03:32:49 +0700

    31/08/2022 Berusaha agar menjadi aplikasi tapi belum

--- UAS_PT/src/main/java/module-info.java
+++ UAS_PT/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.uas_pt {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires jasperreports;
     requires org.hibernate.orm.core;
     requires java.naming;
     requires java.persistence;
